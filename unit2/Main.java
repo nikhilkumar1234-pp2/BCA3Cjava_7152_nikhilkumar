e3bbd7a25a33dcc75b272b5cc27b221efd3f4547
nikhilkumar1234-pp2 <nikhilmonaboti8050@gmail.com>
2026-09-22 15:02:21 +0530

    Delete unit2 directory

--- unit2/Main.java
+++
@@ -1,8 +0,0 @@
-import school.students.Student;
-
-public class Main{
-    public static void main(String args[]){
-        Student s=new Student();
-        s.displayinfo();
-    }
-}
--- unit2/u2_p13.java
+++
@@ -1,9 +0,0 @@
-//Write a java program of package.
-package mypack;
-public class u2_p13
-{
-    public static void main(String[] args)
-    {
-        System.out.println("This is package");
-    }
-}
